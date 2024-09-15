4fc7d475e7ac492abc4a15597db7eea2a555c403
harsh rawat <42332761+tetrex@users.noreply.github.com>
2024-09-15 14:46:00 +0530

    fix: nav fix

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,40 +1,50 @@
-import Link from 'next/link'
+import Link from "next/link";
 
-const navItems = {
-  '/': {
-    name: 'home',
-  },
-  '/blog': {
-    name: 'blog',
-  },
-  'https://vercel.com/templates/next.js/portfolio-starter-kit': {
-    name: 'deploy',
-  },
+interface NavItem {
+	name: string;
+	openInNewTab: boolean;
 }
 
+const navItems: { [key: string]: NavItem } = {
+	"/": {
+		name: "home",
+		openInNewTab: false,
+	},
+	"/blog": {
+		name: "blog",
+		openInNewTab: false,
+	},
+	"/harsh_rawat_resume_2023.pdf": {
+		name: "resume",
+		openInNewTab: true,
+	},
+};
+
 export function Navbar() {
-  return (
-    <aside className="-ml-[8px] mb-16 tracking-tight">
-      <div className="lg:sticky lg:top-20">
-        <nav
-          className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
-          id="nav"
-        >
-          <div className="flex flex-row space-x-0 pr-10">
-            {Object.entries(navItems).map(([path, { name }]) => {
-              return (
-                <Link
-                  key={path}
-                  href={path}
-                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
-                >
-                  {name}
-                </Link>
-              )
-            })}
-          </div>
-        </nav>
-      </div>
-    </aside>
-  )
+	return (
+		<aside className="-ml-[8px] mb-16 tracking-tight">
+			<div className="lg:sticky lg:top-20">
+				<nav
+					className="flex flex-row items-center justify-center relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+					id="nav"
+				>
+					<div className="flex flex-row space-x-0 pr-10">
+						{Object.entries(navItems).map(([path, { name, openInNewTab }]) => {
+							return (
+								<a
+									key={path}
+									href={path}
+									target={openInNewTab ? "_blank" : "_self"} // Open in new tab or the same tab
+									rel={openInNewTab ? "noopener noreferrer" : undefined} // Add security attributes for new tab
+									className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
+								>
+									{name}
+								</a>
+							);
+						})}
+					</div>
+				</nav>
+			</div>
+		</aside>
+	);
 }
